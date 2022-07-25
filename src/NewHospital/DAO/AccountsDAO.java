31404d4285a0f1b8063f9fb117442774eb058dc8
ASUS <ASUS@LAPTOP-J1LGJQ4C>
2022-07-25 14:34:27 +0700

    DAO Account

--- src/NewHospital/DAO/AccountsDAO.java
+++ src/NewHospital/DAO/AccountsDAO.java
@@ -1,5 +1,12 @@
 package NewHospital.DAO;
 
+import ConnectData.JDBC;
+import NewHospital.Model.Accounts;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +18,70 @@ package NewHospital.DAO;
  * @author ASUS
  */
 public class AccountsDAO {
-    
+        //đọc 1 nhân viên từ 1 bản ghi (1 ResultSet)
+        public Accounts readForm(ResultSet rs) throws SQLException{
+                Accounts model = new Accounts();
+                model.setUserName(rs.getString("TaiKhoan"));
+                model.setPassWord(rs.getString("MatKhau"));
+                model.setHoTen(rs.getString("HoTen"));
+                model.setSoDT(rs.getString("SoDT"));
+                model.setVaiTro(rs.getBoolean("Role"));
+                return model;
+        }
+        //thực hiện truy vấn lấy về 1 tập ResultSet
+        public List<Accounts> selects(String sql,Object...args){
+        List<Accounts> list=new ArrayList<>();
+        try {
+            ResultSet rs=null;
+            try{
+                rs=JDBC.executeQuery(sql, args);
+                while(rs.next()){
+                    list.add(readForm(rs));
+                }
+            }finally{
+                rs.getStatement().getConnection().close();      //đóng kết nối từ resultSet
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException();
+        }
+        return list;
+        }
+        // Add vào CSDL
+        public void insert(Accounts entity){
+            String sql = "Insert into ListAccount(TaiKhoan, MatKhau, HoTen, SoDT, Role) values (?,?,?,?,?)";
+            JDBC.executeUpdate(sql,
+                    entity.getUserName(),
+                    entity.getPassWord(),
+                    entity.getHoTen(),
+                    entity.getSoDT(),
+                    entity.isVaiTro()
+            );
+        }
+        // Capnhat vào CSDL
+        public void update(Accounts entity){
+            String sql = "Update ListAccount SET  MatKhau = ?, HoTen=?,SoDT = ?, VaiTro = ? WHERE TaiKhoan = ?";
+            JDBC.executeUpdate(sql,
+                    entity.getPassWord(),
+                    entity.getHoTen(),
+                    entity.getSoDT(),
+                    entity.isVaiTro(),
+                    entity.getUserName()
+            );
+        }
+        // Xóa bảng ghi ra khỏi CSDL
+        public void delete(String tk){
+            String sql="DELETE FROM ListAccount WHERE TaiKhoan=?";
+            JDBC.executeUpdate(sql, tk);
+        }
+        // Truy vấn show database bảng NhânViên
+        public List<Accounts> selects() {
+        String sql="SELECT * FROM ListAccount";
+        return selects(sql);      //trong 1 class có thể có 2 method trùng tên (nhưng param khác nhau)
+        }
+        // Tìm nhânViên theo Id (MaNV)
+        public Accounts FindIdNV(String id){
+            String sql = "SELECT * FROM ListAccount WHERE TaiKhoan = ?";
+            List<Accounts> list=selects(sql, id);
+            return list.size()>0?list.get(0):null;   //có thể trả về là null
+        }
 }
